eb7059fd7dc8a1957e63941afe814b1db4584e27
Vivek Pavaskar <vivek.pavaskar@gmail.com>
2020-08-23 15:14:59 +0530

    burger display almost complete

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -1,16 +1,20 @@
 import React from 'react';
 import BurgerIngredient from './BurgerIngredent/BurgerIngredient';
 import './Burger.css';
-import { array } from 'prop-types';
 
 const burger = (props) =>{
-    const ingredient=Object.keys(props.ingredient).map( igkey =>{
+    let ingredient=Object.keys(props.ingredient).map( igkey =>{
         return [...Array(props.ingredient[igkey])].map((_,i) =>{
                 return <BurgerIngredient key={igkey+i} type={igkey}></BurgerIngredient>
         })
     })
+    .reduce((arr,el)=>{
+        return arr.concat(el)
+    },[]);
 
-
+    if(ingredient.length===0){
+        ingredient=<p>Please Start adding Ingredient!</p>
+    }
         return(
             <div className='Burger'>
                 <BurgerIngredient type='bread-top'></BurgerIngredient>
